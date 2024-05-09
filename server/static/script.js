92d9e4907d6fbf63a87bfb63afdd71215ee9860a
DanJenkins-Developer <djenkinsjg0617@gmail.com>
2024-05-09 00:23:21 -0500

    the best weather app

--- server/static/script.js
+++ server/static/script.js
@@ -4,6 +4,9 @@ const button = document.getElementById("button")
 const feelLikeId = document.getElementById("feels")
 const cityId = document.getElementById("city")
 const countryId = document.getElementById("country")
+const temperature = document.getElementById("temp")
+
+const picture = document.getElementById("pic")
 
 button.addEventListener("click", getCurrentWeather);
 
@@ -27,21 +30,25 @@ function getCurrentWeather() {
     //feelLikeId.innerHTML = "F U"
     cityId.innerHTML = data.name
     countryId.innerHTML = data.sys.country
+    temperature.innerHTML = data.main.temp
 
     if (data.main.temp < 69)
       {
         //alert("Cold af")
         feelLikeId.innerHTML = " Cold af"
+        picture.src = "./assets/wtf.png"
       }
     else if (data.main.temp >= 69 || data.main.temp <= 78)
     {
       //alert("Nice")
       feelLikeId.innerHTML = data.main.temp + " NICE"
+      picture.src = "./assets/nice.png"
     }
     else if(data.main.temp > 78)
       {
         //alert("Ohhhh hot")
         feelLikeId.innerHTML = data.main.temp + " Ohhhh hot"
+        picture.src = "./assets/hot.png"
       }
   })
   .catch(error => {

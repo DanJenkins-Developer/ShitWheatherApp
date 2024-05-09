94dba968fdfe4d78132c9d06b065b513c47c4595
emilyastranova <emily@astralabs.dev>
2024-05-08 22:11:24 -0500

    Simple server done
    Signed-off-by: emilyastranova <emily@astralabs.dev>

---
+++ server/main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+from fastapi import FastAPI
+import requests
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+try:
+    OPENWEATHER_API_KEY = os.getenv("OPENWEATHER_API_KEY")
+except KeyError:
+    print("Please set the OPENWEATHER_API_KEY environment variable")
+    sys.exit(1)
+CITY = "College Station"
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to ShitWeatherApp"}
+
+@app.get("/weather")
+async def weather():
+    # Get weather data from OpenWeather API
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={OPENWEATHER_API_KEY}&units=imperial"
+    response = requests.get(url)
+    data = response.json()
+    return data

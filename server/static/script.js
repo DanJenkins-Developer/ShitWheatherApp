b4082bc492afe3a0ab3e99218b1309b9aa9f81bc
emilyastranova <emily@astralabs.dev>
2024-05-09 01:03:21 -0500

    Update messages
    Signed-off-by: emilyastranova <emily@astralabs.dev>

--- server/static/script.js
+++ server/static/script.js
@@ -1,60 +1,46 @@
-const baseUrl = 'http://localhost:8000';
+const button = document.getElementById("button");
+const feelLikeId = document.getElementById("feels");
+const cityId = document.getElementById("city");
+const countryId = document.getElementById("country");
+const temperature = document.getElementById("temp");
 
-const button = document.getElementById("button")
-const feelLikeId = document.getElementById("feels")
-const cityId = document.getElementById("city")
-const countryId = document.getElementById("country")
-const temperature = document.getElementById("temp")
-
-const picture = document.getElementById("pic")
+const picture = document.getElementById("pic");
 
 button.addEventListener("click", getCurrentWeather);
 
 function getCurrentWeather() {
+	const path = "/api/weather";
 
-  
-  const path = "/api/weather"
-  
-  const apiUrl = baseUrl + path
-  
-  fetch(apiUrl)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-    // feelLikeId.innerHTML = data.weather[0].description
-    //feelLikeId.innerHTML = "F U"
-    cityId.innerHTML = data.name
-    countryId.innerHTML = data.sys.country
-    temperature.innerHTML = data.main.temp
+	fetch(path)
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+			return response.json();
+		})
+		.then((data) => {
+			console.log(data);
+			cityId.innerHTML = data.name;
+			countryId.innerHTML = data.sys.country;
+			temperature.innerHTML = data.main.temp;
 
-    if (data.main.temp < 69)
-      {
-        //alert("Cold af")
-        feelLikeId.innerHTML = " Cold af"
-        picture.src = "./assets/wtf.png"
-      }
-    else if (data.main.temp >= 69 || data.main.temp <= 78)
-    {
-      //alert("Nice")
-      feelLikeId.innerHTML = data.main.temp + " NICE"
-      picture.src = "./assets/nice.png"
-    }
-    else if(data.main.temp > 78)
-      {
-        //alert("Ohhhh hot")
-        feelLikeId.innerHTML = data.main.temp + " Ohhhh hot"
-        picture.src = "./assets/hot.png"
-      }
-  })
-  .catch(error => {
-    console.error('Error:', error);
-    alert ("Get Fucked idiot!");
-  });
+			if (data.main.temp < 69) {
+				feelLikeId.innerHTML = " Cold af";
+				picture.src = "./assets/wtf.png";
+			} else if (data.main.temp >= 69 && data.main.temp < 70) {
+				feelLikeId.innerHTML = data.main.temp + " NICE";
+				picture.src = "./assets/nice.png";
+			} else if (data.main.temp >= 70) {
+				feelLikeId.innerHTML = data.main.temp + " Ohhhh hot";
+				picture.src = "./assets/hot.png";
+			}
+        else {
+            feelLikeId.innerHTML = data.main.temp + " Hot af";
+            picture.src = "./assets/hot.png";
+        }
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+			alert("Get Fucked idiot!");
+		});
 }
-  
-
